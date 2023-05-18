ab885d29515646a876c59496e1ec0ff138222d2b
DavidZ <davidbill@outlook.com>
2023-05-18 14:00:34 +1000

    ✨ Update url to paper

--- src/views/TeaserView.vue
+++ src/views/TeaserView.vue
@@ -13,7 +13,9 @@
       </div>
     </div>
     <div class="px-10 md:px-40 pt-[67.2px] xl:pt-[84px] absolute inset-0 flex justify-center items-center">
-      <div class="p-6 md:p-10 text-center text-primary text-bold bg-white/30 rounded-xl backdrop-blur-xl backdrop-sepia">
+      <div
+        class="p-6 md:p-10 text-center text-primary text-bold bg-white/30 rounded-xl backdrop-blur-xl backdrop-sepia"
+      >
         <div class="font-extrabold text-2xl md:text-6xl">IKEA ASSEMBLY IN THE WILD (IAW) DATASET</div>
         <a
           class="inline-block pt-2 md:pt-8 text-sm md:text-3xl font-bold hover:underline"
@@ -22,7 +24,9 @@
           >Aligning Step-by-Step Instruction Diagrams to Video Demonstrations</a
         >
         <div class="py-2 md:py-16 flex flex-wrap justify-center">
-          <a href="https://arxiv.org/pdf/2303.13800.pdf" target="_blank"
+          <a
+            href="https://openaccess.thecvf.com/content/CVPR2023/papers/Zhang_Aligning_Step-by-Step_Instructional_Diagrams_to_Video_Demonstrations_CVPR_2023_paper.pdf"
+            target="_blank"
             ><button class="btn"><NewspaperIcon class="h-4 md:h-6 md:mr-1" />Paper</button></a
           >
           <a href="#download"
@@ -50,7 +54,10 @@
           <a href="https://academic.davidz.cn/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
             >Jiahao Zhang<sup>1</sup></a
           >
-          <a href="https://users.cecs.anu.edu.au/~cherian/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
+          <a
+            href="https://users.cecs.anu.edu.au/~cherian/"
+            class="inline-block px-2 md:px-6 hover:underline"
+            target="_blank"
             >Anoop Cherian<sup>2</sup></a
           >
           <a href="https://csyanbin.github.io/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
@@ -63,7 +70,10 @@
           <a href="https://crodriguezo.github.io/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
             >Cristian Rodriguez<sup>4</sup></a
           >
-          <a href="https://users.cecs.anu.edu.au/~sgould/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
+          <a
+            href="https://users.cecs.anu.edu.au/~sgould/"
+            class="inline-block px-2 md:px-6 hover:underline"
+            target="_blank"
             >Stephen Gould<sup>1</sup></a
           >
         </div>
@@ -80,12 +90,18 @@
           </div>
           <div class="lg:w-full"></div>
           <div>
-            <a href="https://www.technion.ac.il/en/home-2/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
+            <a
+              href="https://www.technion.ac.il/en/home-2/"
+              class="inline-block px-2 md:px-6 hover:underline"
+              target="_blank"
               ><sup>3</sup>Technion Israel Institute of Technology</a
             >
           </div>
           <div>
-            <a href="https://www.adelaide.edu.au/aiml/" class="inline-block px-2 md:px-6 hover:underline" target="_blank"
+            <a
+              href="https://www.adelaide.edu.au/aiml/"
+              class="inline-block px-2 md:px-6 hover:underline"
+              target="_blank"
               ><sup>4</sup>The Australian Institute for Machine Learning</a
             >
           </div>
